95141be743d115ef8f1d0122017c09e7d091d5d0
gabe-batres <36239493+gabe-batres@users.noreply.github.com>
2025-01-14 11:57:50 +0100

    Update Code.R

--- Code.R
+++ Code.R
@@ -17,19 +17,19 @@
 # hsgrad_b:  
 # somecoll_b:  
 # collgrad_b:  
-# povrate_w:  
-# povrate_b:  
-# mt1proom_w:  
-# mt1proom_b:  
-# medgrent_w:  
-# medgrent_b:  
-# medgrentpinc_w:  
-# medgrentpinc_b:  
-# area1910:  Total area - 1910?
+# povrate_w:  Poverty rate for whites
+# povrate_b:  Poverty rate for blacks
+# mt1proom_w:  Share of households with more than one person per room, White
+# mt1proom_b:  Share of households with more than one person per room, Black
+# medgrent_w:  Median rent, White
+# medgrent_b:  Median rent, Black
+# medgrentpinc_w:  Median rent as a percent of income, White
+# medgrentpinc_b:  Median rentas a percent of income, Black
+# area1910:  Total area - 1910
 # passpc:  Per capita street car passengers–1915
 # ngov62:  Number of local governments–1962
 # manshr:  Manufacturing share–1990
-# incseg:  Income segregation–1990 ?
+# incseg:  Income segregation–1990
 # closeness:  Distance from south
 # regdum1:  Northeast region indicator
 # regdum2:  Midwest region indicator
@@ -47,8 +47,8 @@
 # ctyrail_wkrs1920:  
 # count1920:  
 # black1920:  
-# gini_w:  
-# gini_b:  
+# gini_w:  Gini index for whites
+# gini_b:  Gini index for blacks
 # p10_w:  
 # p50_w:  50th percentile income for whites ?
 # p90_w:  
@@ -64,10 +64,10 @@
 # ln10w10b:  
 # ln90w10b:  
 # ln90b10w:  
-# mv_st_winus_w:  
-# mv_st_winus_b:  
-# lfp_w:  Labor force participation (white)
-# lfp_b:  Labor force participation (black)
+# mv_st_winus_w:  Percent of residents who are in-migrants, White
+# mv_st_winus_b:  Percent of residents who are in-migrants, Black
+# lfp_w:  
+# lfp_b:  
 # dism1990:  Dissimilarity index–1990
 # pop1990:  Population–1990
 # pctbk1990:  Percent black–1990
@@ -90,50 +90,53 @@ library(AER)  # Library for 2SLS
 # Import the data
 data <- read_dta("ananat2011.dta")
 
+
 # Step 1: Investigate data ==========================================
+# Understand the raw data provided
 
-# Understand the data provided
-
-class(df)
-head(df)
+class(data)
+head(data)
 
 # What is the dimension of the data
-dim(df)
+dim(data)
 
 
 # Each row is a "Metropolitan Statistical Area" (MSA)
-non_numeric_cols <- names(df)[!sapply(df, is.numeric)]
-print(sapply(df[non_numeric_cols], function(x) length(unique(x))))
+non_numeric_cols <- names(data)[!sapply(data, is.numeric)]
+print(sapply(data[non_numeric_cols], function(x) length(unique(x))))
 rm(non_numeric_cols)
 
 # Get names of numeric columns
-numeric_cols <- names(df)[sapply(df, is.numeric)]
+numeric_cols <- names(data)[sapply(data, is.numeric)]
 
 # Create a summary table to see the basics of the data.
 summary_table <- data.frame(
   Column_Name = numeric_cols,
-  Number_of_Rows = nrow(df),
-  Number_of_Unique_Values = sapply(df[numeric_cols], function(x) length(unique(x))),
-  Min = round(sapply(df[numeric_cols], min, na.rm = TRUE), 5),
-  Max = round(sapply(df[numeric_cols], max, na.rm = TRUE), 5),
-  Mean = round(sapply(df[numeric_cols], mean, na.rm = TRUE), 5),
-  Median = round(sapply(df[numeric_cols], median, na.rm = TRUE), 5),
-  Standard_Deviation = round(sapply(df[numeric_cols], sd, na.rm = TRUE), 5)
+  Number_of_Rows = nrow(data),
+  Number_of_Unique_Values = sapply(data[numeric_cols], function(x) length(unique(x))),
+  Min = round(sapply(data[numeric_cols], min, na.rm = TRUE), 5),
+  Max = round(sapply(data[numeric_cols], max, na.rm = TRUE), 5),
+  Mean = round(sapply(data[numeric_cols], mean, na.rm = TRUE), 5),
+  Median = round(sapply(data[numeric_cols], median, na.rm = TRUE), 5),
+  Standard_Deviation = round(sapply(data[numeric_cols], sd, na.rm = TRUE), 5)
 )
 
+
 # Function to search and find the columns needed to reproduce the analysis
-find_cols <- function(string) {
+find_columns <- function(string) {
   return(
     colnames(data)[grepl(string, names(data), ignore.case = TRUE)]
   )
 }
 
 # Example usage
-find_cols("gini")
+columns_with_word <- find_columns("gini")
+print(find_columns("1920"))
+print(find_columns("area"))
 
 # The regdum columns appear to indicate what region the city is in
-print(sum(rowSums(data[find_cols("regdum")])))
-colSums(data[find_cols("regdum")])
+print(sum(rowSums(data[find_columns("regdum")])))
+colSums(data[find_columns("regdum")])
 
 
 # Step 2: Scatter Plots with Linear Regression Line =================
@@ -150,7 +153,7 @@ create_scatter_plot <- function(df, x_col, y_col) {
   ggplot(df, aes(x = .data[[x_col]], y = .data[[y_col]])) +
     geom_point(color = "darkblue") +
     geom_smooth(method = "lm", se = FALSE, color = "darkred") +
-    geom_text(aes(label = name), size = 2, color = "gray", alpha = 0.8,
+    geom_text(aes(label = name), size = 2, color = "darkgray", alpha = 0.6,
               hjust = 0.5, vjust = 1.5) +
     xlim(0.2, 1) +
     ylim(0.2, 1) +
@@ -198,11 +201,11 @@ create_partitioned_scatter_plot <- function(df, x_col, y_col, group_col) {
          caption = "Note: This plot shows the relationship between variables, partitioned by groups.") +
     annotate("text", 
              x = min(df[[x_col]]), y = max(df[[y_col]]), 
-             label = paste("Slope (Top) =", round(slope_top, 2)), 
+             label = paste("Slope (Top) =", round(slope_top, 5)), 
              color = "red3", hjust = 0, vjust = 1) +  # Top line label
     annotate("text", 
              x = min(df[[x_col]]), y = max(df[[y_col]]) - 0.1 * diff(range(data[[y_col]])), 
-             label = paste("Slope (Bottom) =", round(slope_bottom, 2)), 
+             label = paste("Slope (Bottom) =", round(slope_bottom, 5)), 
              color = "blue3", hjust = 0, vjust = 1) +  # Bottom line label (adjusted position)
     theme_bw() +
     scale_color_manual(values = c("red3", "blue3")) +  # Set colors for points
@@ -212,23 +215,24 @@ create_partitioned_scatter_plot <- function(df, x_col, y_col, group_col) {
 create_partitioned_scatter_plot(data, "closeness", "dism1990", "herf")  # Replace with actual column names
 
 
+# Step 3: Regression tables of the paper ============================
 
-# Step 3: Tables of ==================
-
-# Table 1  ----------------------------------------------------------------------------
-
+# Table 1 - Testing RDI as an Instrument ----------------------------
+# Divide column by 1,000
 data$area1910 <- data$area1910/1000
 data$count1910 <- data$count1910/1000
 data$passpc <- data$passpc/1000
 
 # Dependent Variables
-vars <- c("dism1990", "area1910", "count1910", "ethseg10", "ethiso10", "black1910", 
-          "passpc", "black1920", "ctyliterate1920", "lfp1920", "ctytrade_wkrs1920", 
-          "ctymanuf_wkrs1920", "ctyrail_wkrs1920", "incseg")
+focus_variables1 <- c("dism1990")
+false_variables1 <- c("area1910", "count1910", "ethseg10", "ethiso10", "black1910", "passpc",
+                      "black1920", "ctyliterate1920", "lfp1920", "ctytrade_wkrs1920", "ctymanuf_wkrs1920", "ctyrail_wkrs1920",
+                      "incseg")
+vars <- c(focus_variables1, false_variables1)
 
-# Run regressions
+# Run OLS regressions for each variable
 models <- lapply(vars, function(var) {
-  lm(as.formula(paste(var, "~ herf + lenper")), data)
+  lm(as.formula(paste(var, "~ herf + lenper")), data = data)
 })
 
 # Compute robust standard errors
@@ -264,7 +268,6 @@ stargazer(models[1:7],
           add.lines = list(means1),
           omit.stat = c("rsq", "adj.rsq", "ser", "f")
 )
-
 # Generate the lower table
 stargazer(models[8:14],
           omit = "(Constant)",
@@ -288,7 +291,7 @@ stargazer(models[8:14],
 )
 
 
-# Table 2 -----------------------------------------------------------------------
+# Table 2 - The Effects of Segregation on Poverty and Inequality among Blacks and Whites
 
 # Top part
 m1 <- lm(lngini_w ~ dism1990, data)
@@ -349,7 +352,7 @@ m12 <- lm(ln90b10w ~ herf + lenper, data %>% filter(closeness <= -400))
 coeftest(m12, vcov = vcovHC(m12, type = "HC1"))
 
 
-# Table 3 -----------------------------------------------------------------------
+# Table 3 — Robustness Checks ---------------------------------------
 
 # 1990 Controls:
 
@@ -477,9 +480,225 @@ m4 <- ivreg(povrate_b ~ dism1990 + lenper  + herfscore| herf + lenper + herfscor
 coeftest(m4, vcov = vcovHC(m4, type = "HC1"))
 
 
-# Table 5 -----------------------------------------------------------------------
+# Table 4 — The Effects of 1990 Segregation on 1990 City Demand -----
+
+# Outcome: Percent of residents who are in-migrants
+# OLS
+in_w_ols <- lm(mv_st_winus_w ~ dism1990, data)
+# round(coeftest(in_w_ols, vcov = vcovHC(in_w_ols, type = "HC1")), 3)
+in_b_ols <- lm(mv_st_winus_b ~ dism1990, data)
+# round(coeftest(in_b_ols, vcov = vcovHC(in_b_ols, type = "HC1")), 3)
+# IV
+in_w_iv <- ivreg(mv_st_winus_w ~ dism1990 + lenper | herf + lenper, data = data)
+# round(coeftest(in_w_iv, vcov = vcovHC(in_w_iv, type = "HC1")), 3)
+in_b_iv <- ivreg(mv_st_winus_b ~ dism1990 + lenper | herf + lenper, data = data)
+# round(coeftest(in_b_iv, vcov = vcovHC(in_b_iv, type = "HC1")), 3)
+# Falsification: Reduced form effect of RDI among cities far from the South
+in_w_f <- ivreg(mv_st_winus_w ~ dism1990 + lenper | herf + lenper, data = data %>% filter(closeness < -400))
+# round(coeftest(in_w_f, vcov = vcovHC(in_w_f, type = "HC1")), 3)
+in_b_f <- ivreg(mv_st_winus_b ~ dism1990 + lenper | herf + lenper, data = data %>% filter(closeness < -400))
+# round(coeftest(in_b_f, vcov = vcovHC(in_b_f, type = "HC1")), 3)
+
+# Outcome: Median rent
+# OLS
+mr_w_ols <- lm(medgrent_w ~ dism1990, data)
+# round(coeftest(mr_w_ols, vcov = vcovHC(mr_w_ols, type = "HC1")), 0)
+mr_b_ols <- lm(medgrent_b ~ dism1990, data)
+# round(coeftest(mr_b_ols, vcov = vcovHC(mr_b_ols, type = "HC1")), 0)
+# IV
+mr_w_iv <- ivreg(medgrent_w ~ dism1990 + lenper | herf + lenper, data = data)
+# round(coeftest(mr_w_iv, vcov = vcovHC(mr_w_iv, type = "HC1")), 0)
+mr_b_iv <- ivreg(medgrent_b ~ dism1990 + lenper | herf + lenper, data = data)
+# round(coeftest(mr_b_iv, vcov = vcovHC(mr_b_iv, type = "HC1")), 0)
+# Falsification: Reduced form effect of RDI among cities far from the South
+mr_w_f <- ivreg(medgrent_w ~ dism1990 + lenper | herf + lenper, data = data %>% filter(closeness < -400))
+# round(coeftest(mr_w_f, vcov = vcovHC(mr_w_f, type = "HC1")), 0)
+mr_b_f <- ivreg(medgrent_b ~ dism1990 + lenper | herf + lenper, data = data %>% filter(closeness < -400))
+# round(coeftest(mr_b_f, vcov = vcovHC(mr_b_f, type = "HC1")), 0)
+
+# Outcome: Median rent as a percent of income
+# OLS
+mrpi_w_ols <- lm(medgrentpinc_w ~ dism1990, data)
+# round(coeftest(mrpi_w_ols, vcov = vcovHC(mrpi_w_ols, type = "HC1")), 3)
+mrpi_b_ols <- lm(medgrentpinc_b ~ dism1990, data)
+# round(coeftest(mrpi_b_ols, vcov = vcovHC(mrpi_b_ols, type = "HC1")), 3)
+# IV
+mrpi_w_iv <- ivreg(medgrentpinc_w ~ dism1990 + lenper | herf + lenper, data = data)
+# round(coeftest(mrpi_w_iv, vcov = vcovHC(mrpi_w_iv, type = "HC1")), 3)
+mrpi_b_iv <- ivreg(medgrentpinc_b ~ dism1990 + lenper | herf + lenper, data = data)
+# round(coeftest(mrpi_b_iv, vcov = vcovHC(mrpi_b_iv, type = "HC1")), 3)
+# Falsification: Reduced form effect of RDI among cities far from the South
+mrpi_w_f <- ivreg(medgrentpinc_w ~ dism1990 + lenper | herf + lenper, data = data %>% filter(closeness < -400))
+# round(coeftest(mrpi_w_f, vcov = vcovHC(mrpi_w_f, type = "HC1")), 3)
+mrpi_b_f <- ivreg(medgrentpinc_b ~ dism1990 + lenper | herf + lenper, data = data %>% filter(closeness < -400))
+# round(coeftest(mrpi_b_f, vcov = vcovHC(mrpi_b_f, type = "HC1")), 3)
+
+# Outcome: Share of households with more than one person per room
+# OLS
+mt_w_ols <- lm(mt1proom_w ~ dism1990, data)
+# round(coeftest(mt_w_ols, vcov = vcovHC(mt_w_ols, type = "HC1")), 3)
+mt_b_ols <- lm(mt1proom_b ~ dism1990, data)
+# round(coeftest(mt_b_ols, vcov = vcovHC(mt_b_ols, type = "HC1")), 3)
+# IV
+mt_w_iv <- ivreg(mt1proom_w ~ dism1990 + lenper | herf + lenper, data = data)
+# round(coeftest(mt_w_iv, vcov = vcovHC(mt_w_iv, type = "HC1")), 3)
+mt_b_iv <- ivreg(mt1proom_b ~ dism1990 + lenper | herf + lenper, data = data)
+# round(coeftest(mt_b_iv, vcov = vcovHC(mt_b_iv, type = "HC1")), 3)
+# Falsification: Reduced form effect of RDI among cities far from the South
+mt_w_f <- lm(mt1proom_w ~ dism1990 + lenper, data = data %>% filter(closeness < -400))
+# round(coeftest(mt_w_f, vcov = vcovHC(mt_w_f, type = "HC1")), 3)
+mt_b_f <- lm(mt1proom_b ~ dism1990 + lenper, data = data %>% filter(closeness < -400))
+# round(coeftest(mt_b_f, vcov = vcovHC(mt_b_f, type = "HC1")), 3)
+
+# Define function to extract robust standard errors
+robust_se <- function(model) {
+  return(
+    sqrt(diag(vcovHC(model, type = "HC1")))
+  )
+}
+
+
+# Define a funciton to recreat similar visualizations
+table4_generate <- function(w_model, b_model, col_label, cov_label, omit = "(Constant)|lenper") {
+  return(
+    stargazer(
+      w_model, b_model,
+      se = list(robust_se(w_model), robust_se(b_model)),  # Use robust SE
+      type = "text",
+      title = "OLS, IV, and Falsification Analysis for Table 4",
+      dep.var.labels = c("White", "Black"),
+      column.labels = col_label,
+      column.separate = 2,  # Group each dependent variable into its own block
+      covariate.labels = cov_label,
+      omit = omit,  # Omit other variables (intercept, control variables)
+      omit.stat = c("rsq", "adj.rsq", "ser", "f"),  # Omit statistics
+      notes = "Dependent variable: Segregation. Additional controls omitted for brevity.",  # Add note
+      align = TRUE
+    )
+  )
+}
+# Summarize results using stargazer
+#  - The stargazer package cannot directly create the same tables,
+#    so I am breaking up the output and recombining later in the paper.
+# OLS Row
+table4_generate(in_w_ols, in_b_ols, "In-Migration", "OLS")
+table4_generate(mr_w_ols, mr_b_ols, "Median Rent", "OLS")
+table4_generate(mrpi_w_ols, mrpi_b_ols, "Rent Income", "OLS")
+table4_generate(mt_w_ols, mt_b_ols, "Room", "OLS")
+
+# IV Row
+table4_generate(in_w_iv, in_b_iv, "In-Migration", "Instrument Variable")
+table4_generate(mr_w_iv, mr_b_iv, "Median Rent", "Instrument Variable")
+table4_generate(mrpi_w_iv, mrpi_b_iv, "Rent Income", "Instrument Variable")
+table4_generate(mt_w_iv, mt_b_iv, "Room", "Instrument Variable")
+
+# Falsification Row
+table4_generate(in_w_f, in_b_f, "In-Migration", "Falsification")
+table4_generate(mr_w_f, mr_b_f, "Median Rent", "Falsification")
+table4_generate(mrpi_w_f, mrpi_b_f, "Rent Income", "Falsification")
+table4_generate(mt_w_f, mt_b_f, "Room", "Falsification")
+
+
+# Table 5 — The Effects of 1980 Dissimilarity on Human Capital of 22- to 30-Year-Olds in 1980
+
+# Outcome: Share who are high school dropouts
+# OLS
+hsd_w_ols <- lm(hsdrop_w ~ dism1990, data)
+round(coeftest(hsd_w_ols, vcov = vcovHC(hsd_w_ols, type = "HC1")), 3)
+hsd_b_ols <- lm(hsdrop_b ~ dism1990, data)
+round(coeftest(hsd_b_ols, vcov = vcovHC(hsd_b_ols, type = "HC1")), 3)
+# IV
+hsd_w_iv <- ivreg(hsdrop_w ~ dism1990 + lenper | herf + lenper, data = data)
+round(coeftest(hsd_w_iv, vcov = vcovHC(hsd_w_iv, type = "HC1")), 3)
+hsd_b_iv <- ivreg(hsdrop_b ~ dism1990 + lenper | herf + lenper, data = data)
+round(coeftest(hsd_b_iv, vcov = vcovHC(hsd_b_iv, type = "HC1")), 3)
+# Falsification: Reduced form effect of RDI among cities far from the South
+hsd_w_f <- ivreg(hsdrop_w ~ dism1990 + lenper | herf + lenper, data = data %>% filter(closeness < -400))
+round(coeftest(hsd_w_f, vcov = vcovHC(hsd_w_f, type = "HC1")), 3)
+hsd_b_f <- ivreg(hsdrop_b ~ dism1990 + lenper | herf + lenper, data = data %>% filter(closeness < -400))
+round(coeftest(hsd_b_f, vcov = vcovHC(hsd_b_f, type = "HC1")), 3)
+
+# Outcome: Share who are high school graduates
+# OLS
+hsg_w_ols <- lm(hsgrad_w ~ dism1990, data)
+round(coeftest(hsg_w_ols, vcov = vcovHC(hsg_w_ols, type = "HC1")), 0)
+hsg_b_ols <- lm(hsgrad_b ~ dism1990, data)
+round(coeftest(hsg_b_ols, vcov = vcovHC(hsg_b_ols, type = "HC1")), 0)
+# IV
+hsg_w_iv <- ivreg(hsgrad_w ~ dism1990 + lenper | herf + lenper, data = data)
+round(coeftest(hsg_w_iv, vcov = vcovHC(hsg_w_iv, type = "HC1")), 0)
+hsg_b_iv <- ivreg(hsgrad_b ~ dism1990 + lenper | herf + lenper, data = data)
+round(coeftest(hsg_b_iv, vcov = vcovHC(hsg_b_iv, type = "HC1")), 0)
+# Falsification: Reduced form effect of RDI among cities far from the South
+hsg_w_f <- ivreg(hsgrad_w ~ dism1990 + lenper | herf + lenper, data = data %>% filter(closeness < -400))
+round(coeftest(hsg_w_f, vcov = vcovHC(hsg_w_f, type = "HC1")), 0)
+hsg_b_f <- ivreg(hsgrad_b ~ dism1990 + lenper | herf + lenper, data = data %>% filter(closeness < -400))
+round(coeftest(hsg_b_f, vcov = vcovHC(hsg_b_f, type = "HC1")), 0)
+
+# Outcome: Share who have some college
+# OLS
+sc_w_ols <- lm(somecoll_w ~ dism1990, data)
+round(coeftest(sc_w_ols, vcov = vcovHC(sc_w_ols, type = "HC1")), 3)
+sc_b_ols <- lm(somecoll_b ~ dism1990, data)
+round(coeftest(sc_b_ols, vcov = vcovHC(sc_b_ols, type = "HC1")), 3)
+# IV
+sc_w_iv <- ivreg(somecoll_w ~ dism1990 + lenper | herf + lenper, data = data)
+round(coeftest(sc_w_iv, vcov = vcovHC(sc_w_iv, type = "HC1")), 3)
+sc_b_iv <- ivreg(somecoll_b ~ dism1990 + lenper | herf + lenper, data = data)
+round(coeftest(sc_b_iv, vcov = vcovHC(sc_b_iv, type = "HC1")), 3)
+# Falsification: Reduced form effect of RDI among cities far from the South
+sc_w_f <- ivreg(somecoll_w ~ dism1990 + lenper | herf + lenper, data = data %>% filter(closeness < -400))
+round(coeftest(sc_w_f, vcov = vcovHC(sc_w_f, type = "HC1")), 3)
+sc_b_f <- ivreg(somecoll_b ~ dism1990 + lenper | herf + lenper, data = data %>% filter(closeness < -400))
+round(coeftest(sc_b_f, vcov = vcovHC(sc_b_f, type = "HC1")), 3)
+
+# Outcome: Share who are college graduates
+# OLS
+cg_w_ols <- lm(collgrad_w ~ dism1990, data)
+round(coeftest(cg_w_ols, vcov = vcovHC(cg_w_ols, type = "HC1")), 3)
+cg_b_ols <- lm(collgrad_b ~ dism1990, data)
+round(coeftest(cg_b_ols, vcov = vcovHC(cg_b_ols, type = "HC1")), 3)
+# IV
+cg_w_iv <- ivreg(collgrad_w ~ dism1990 + lenper | herf + lenper, data = data)
+round(coeftest(cg_w_iv, vcov = vcovHC(cg_w_iv, type = "HC1")), 3)
+cg_b_iv <- ivreg(collgrad_b ~ dism1990 + lenper | herf + lenper, data = data)
+round(coeftest(cg_b_iv, vcov = vcovHC(cg_b_iv, type = "HC1")), 3)
+# Falsification: Reduced form effect of RDI among cities far from the South
+cg_w_f <- lm(collgrad_w ~ dism1990 + lenper, data = data %>% filter(closeness < -400))
+round(coeftest(cg_w_f, vcov = vcovHC(cg_w_f, type = "HC1")), 3)
+cg_b_f <- lm(collgrad_b ~ dism1990 + lenper, data = data %>% filter(closeness < -400))
+round(coeftest(cg_b_f, vcov = vcovHC(cg_b_f, type = "HC1")), 3)
+
+
+# Summarize results using stargazer
+#  - The stargazer package cannot directly create the same tables,
+#    so I am breaking up the output and recombining later in the paper.
+# OLS Row
+table4_generate(hsd_w_ols, hsd_b_ols, "high school dropouts", "OLS")
+table4_generate(hsg_w_ols, hsg_b_ols, "high school graduates", "OLS")
+table4_generate(sc_w_ols, sc_b_ols, "some college", "OLS")
+table4_generate(cg_w_ols, cg_b_ols, "college graduates", "OLS")
+
+# IV Row
+table4_generate(hsd_w_iv, hsd_b_iv, "high school dropouts", "Instrument Variable")
+table4_generate(hsg_w_iv, hsg_b_iv, "high school graduates", "Instrument Variable")
+table4_generate(sc_w_iv, sc_b_iv, "some college", "Instrument Variable")
+table4_generate(cg_w_iv, cg_b_iv, "college graduates", "Instrument Variable")
+
+# Falsification Row
+table4_generate(hsd_w_f, hsd_b_f, "high school dropouts", "Falsification")
+table4_generate(hsg_w_f, hsg_b_f, "high school graduates", "Falsification")
+table4_generate(sc_w_f, sc_b_f, "some college", "Falsification")
+table4_generate(cg_w_f, cg_b_f, "college graduates", "Falsification")
+
+
+# Step 4: Additional Analysis =======================================
+  
+
+# Step 5: Results & Conclusions =====================================
+# (Add your code for summarizing and presenting results here)
 
 
 
 
-
+# EoF
